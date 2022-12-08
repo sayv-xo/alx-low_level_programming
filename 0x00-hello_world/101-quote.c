3d3f124fc09a91db0392e9bc569cc4ec4f54f6d2
sayv-xo <savegawugah01@gmail.com>
2022-12-08 16:14:23 +0000

    C program that writes to stderr

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 on success
+ */
+int main(void)
+{
+	write(3, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
