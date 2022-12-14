0b8fe0bb7486c97d5bc1b93b1063b94902722c21
Saviour Gawugah <savegawugah01@gmail.com>
2022-12-13 17:06:20 -0800

    Adds two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - Adds two integers
+ * @i: Integer to add
+ * @j: Integer to add
+ *
+ * Return: The results of integers added
+ */
+
+int add(int i, int j)
+{
+	return (i + j);
+}
