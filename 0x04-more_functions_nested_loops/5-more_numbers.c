975ab913ed38d8500b3abdfbf510484516c6fb83
sayv-xo <savegawugah01@gmail.com>
2022-12-15 16:47:15 +0000

    Modified 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,15 +10,14 @@ void more_numbers(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j <= 14; j++)
+		for (j = 0; j < 15; j++)
 		{
-			if (i >= 10)
+			if (j >= 10)
 			{
-				_putchar((i / 10) + '0');
+				_putchar((j / 10) + '0');
 			}
-			_putchar((i % 10) + '0');
+			_putchar((j % 10) + '0');
 		}
 		_putchar('\n');
 	}
-	_putchar('\n');
 }
