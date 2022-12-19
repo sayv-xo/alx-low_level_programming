0e170b0ae8601940a5e508faeff17af867fb0ad1
sayv-xo <savegawugah01@gmail.com>
2022-12-19 19:38:51 +0000

    Update code to comply with betty documentation

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,14 +2,14 @@
 
 /**
  * _puts - Prints a string followed by a new line
- * str: The string to print
+ * @str: The string to print
  */
 
 void _puts(char *str)
 {
 	int i = 0;
 
-	while(str[i] != '\0')
+	while (str[i] != '\0')
 	{
 		_putchar(str[i]);
 		i++;
