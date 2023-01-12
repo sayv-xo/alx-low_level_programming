e9e609dbcc2086d2e1b554c2512c24af14829ae1
sayv-xo <savegawugah01@gmail.com>
2023-01-12 23:41:12 +0000

    Program that concatenate two strings using n bytes
    The returned pointer is a newly allocated memory which contains s1,
    followed by the first n bytes of s2 and null terminated

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * string_nconcat - Concatenate two strings
+ * @s1: The destination string
+ * @s2: The string to concatenate
+ * @n: Number of bytes to use
+ * Return: The concatenated string, which is a pointer to newly
+ * allocated memory on success, else return NULL
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *str;
+	unsigned int i, j = 0, len = 0;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	while (s1[j] != '\0')
+	{
+		j++;
+		len++;
+	}
+
+	str = malloc(sizeof(char) * len + 1);
+
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; i < n && s1[i] != '\0'; i++)
+	{
+		str[i] = s1[i];
+	}
+	for (i = 0; i < n && s2[i] != '\0'; i++)
+	{
+		str[len + i] = s2[i];
+	}
+	str[len + i] = '\0';
+
+	return (str);
+}
