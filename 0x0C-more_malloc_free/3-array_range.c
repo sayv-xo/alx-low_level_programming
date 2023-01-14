20c6f608f27e8da31a9bebb6601af233a4186ee7
sayv-xo <savegawugah01@gmail.com>
2023-01-14 06:05:55 +0000

    Correction

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -22,7 +22,7 @@ int *array_range(int min, int max)
 	if (arr == NULL)
 		return (NULL);
 
-	for (i = 0; i <= size; i++, min++)
+	for (i = 0; i < size; i++, min++)
 		arr[i] = min;
 
 	return (arr);
