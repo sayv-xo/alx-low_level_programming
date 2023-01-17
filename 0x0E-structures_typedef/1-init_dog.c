dfc20440f667c17bc6069e8f86892b6829f9e2d6
sayv-xo <savegawugah01@gmail.com>
2023-01-17 04:41:13 +0000

    Program to initialize a struct

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+
+/**
+ * init_dog - Initialize a struct
+ * @d: struct dog
+ * @name: Dog name
+ * @age: Dog age
+ * @owner: Dog owner
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
