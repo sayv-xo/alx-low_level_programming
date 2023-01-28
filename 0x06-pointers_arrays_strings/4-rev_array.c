f854e8427173974139cc57af22da7b5e43187030
sayv-xo <savegawugah01@gmail.com>
2023-01-28 08:03:24 +0000

    Function that reverses an array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -2,18 +2,18 @@
 
 /**
  * reverse_array - Function that reverses an array
- * @a: Pointer to int
- * @n: Number of elements in array
+ * @a: Pointer to an int
+ * @n: The number of elements in array
  */
 
 void reverse_array(int *a, int n)
 {
-    int i, tmp;
+	int i, tmp;
 
-    for (i = 0; i < n / 2; i++)
-    {
-        tmp = a[n - 1 - i];
-        a[n - 1 - i] = a[i];
-        a[i] = tmp;
-    }
+	for (i = 0; i < n / 2; i++)
+	{
+		tmp = a[n - 1 - i];
+		a[n - 1 - i] = a[i];
+		a[i] = tmp;
+	}
 }
