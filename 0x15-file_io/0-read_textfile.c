bf5c5151106573aaa6a4e7698287518cf68ffc7c
sayv-xo <savegawugah01@gmail.com>
2023-02-17 01:32:24 +0000

    Fixed errors

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,15 +10,13 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	char *buffer = malloc(sizeof(char) * letters);
-	ssize_t op, wrt;
+	ssize_t op, rd, wrt;
 
-	if (filename == NULL)
-		return (0);
-	if ((op = open(filename, O_RDONLY)) == -1)
-		return (0);
-	if (read(op, buffer, letters) == -1)
-		return (0);
-	if ((wrt = write(STDOUT_FILENO, buffer, letters)) == -1)
+	op = open(filename, O_RDONLY);
+	rd = read(op, buffer, letters);
+	wrt = write(STDOUT_FILENO, buffer, rd);
+
+	if (filename == NULL || op == -1 || rd == -1 || wrt == -1)
 		return (0);
 	free(buffer);
 	close(op);
